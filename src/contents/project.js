ed68bb3bdd91da3854b65a053086c3b5a60ca215
jittapat <idea26746@gmail.com>
2025-02-27 11:26:36 +0700

    add project content ✨

--- src/contents/project.js
+++ src/contents/project.js
@@ -13,4 +13,17 @@ export const data = [
         ],
         skills:[[ "React.js","Tailwind CSS"]],
     },
+    {
+        date: "february 2025",
+        title: "ระบบสร้างเนื้อหาโดยอ้างอิงจากการสืบค้นข้อมูลด้วย Generative AI ",
+        link: "",
+        material: [
+            //{ type:faGithub,link:"https://github.com/jittapat-idea/Portfolio"}
+        ],
+        description: [
+            "Position: AI Developer",
+            "Duties and Responsibilities: I worked on the Intent classification using BERT model. I also worked on Prompt engineering, data collection and evaluation LLM model answer used AlpacaEval."
+        ],
+        skills:[["python","AlpacaEval",]],
+    },
 ]
